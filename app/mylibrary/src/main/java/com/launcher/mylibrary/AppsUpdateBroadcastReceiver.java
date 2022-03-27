a21f830f3b8c09efcaaea8ebd565516d935eabbe
amit lodha <amit04jain@gmail.com>
2022-03-28 03:49:44 +0530

    Toast message added

--- app/mylibrary/src/main/java/com/launcher/mylibrary/AppsUpdateBroadcastReceiver.java
+++ app/mylibrary/src/main/java/com/launcher/mylibrary/AppsUpdateBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 /*Receiver to logs update for install and uninstall apps*/
 public class AppsUpdateBroadcastReceiver extends BroadcastReceiver {
@@ -12,10 +13,10 @@ public class AppsUpdateBroadcastReceiver extends BroadcastReceiver {
         String packageName = intent.getData().getEncodedSchemeSpecificPart();
         switch (intent.getAction()) {
             case Intent.ACTION_PACKAGE_REMOVED:
-
+                Toast.makeText(context,"Uninstall App",Toast.LENGTH_LONG).show();
                 break;
             case Intent.ACTION_PACKAGE_ADDED:
-                Log.e("AppsUpdateBroadcastReceiver", "App Added: ${packageName ?: }");
+                Toast.makeText(context,"Installed App",Toast.LENGTH_LONG).show();
 
                 break;
 
